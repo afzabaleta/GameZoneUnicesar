596b7c3a6f254b38af23de5e90f65643ffdfc207
Sherly Corrales <scorrales@unicesar.edu.co>
2026-09-03 15:20:25 -0500

    feat: add ProductService registration method

---
+++ src/main/java/com/gamezone/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.gamezone.service;
+
+import com.gamezone.model.Product;
+import com.gamezone.persistence.ProductRepository;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ProductService {
+
+    private final ProductRepository repository;
+    private final List<Product> products;
+
+    public ProductService(ProductRepository repository)
+            throws IOException, ClassNotFoundException {
+
+        this.repository = repository;
+        this.products = repository.loadProducts();
+    }
+
+    public void registerProduct(Product product) throws IOException {
+        products.add(product);
+        repository.saveProducts(products);
+    }
+}

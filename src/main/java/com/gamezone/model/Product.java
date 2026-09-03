3938ba706981087d0002dc95ae55b7ac0309c49d
Sherly Corrales <scorrales@unicesar.edu.co>
2026-09-03 09:35:53 -0500

    feat: add Product constructor

--- src/main/java/com/gamezone/model/Product.java
+++ src/main/java/com/gamezone/model/Product.java
@@ -6,4 +6,11 @@ public abstract class Product {
     private String title;
     private double price;
     private int stock;
+
+    public Product(String id, String title, double price, int stock) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.stock = stock;
+    }
 }

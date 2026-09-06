d7c9465f41b2ba514a447b56fd269917aa1e37d3
damestre <diegomestre98@gmail.com>
2026-09-06 18:05:00 -0500

    feat: add Person constructor and accessors

--- src/main/java/com/gamezone/model/Person.java
+++ src/main/java/com/gamezone/model/Person.java
@@ -8,4 +8,71 @@ public abstract class Person {
     private String name;
     private String identification;
     private String phone;
+
+    /**
+     * Creates a person with the specified information.
+     *
+     * @param name the person's name
+     * @param identification the person's identification
+     * @param phone the person's phone number
+     */
+    public Person(String name, String identification, String phone) {
+        this.name = name;
+        this.identification = identification;
+        this.phone = phone;
+    }
+
+    /**
+     * Returns the person's name.
+     *
+     * @return the person's name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Updates the person's name.
+     *
+     * @param name the new name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the person's identification.
+     *
+     * @return the person's identification
+     */
+    public String getIdentification() {
+        return identification;
+    }
+
+    /**
+     * Updates the person's identification.
+     *
+     * @param identification the new identification
+     */
+    public void setIdentification(String identification) {
+        this.identification = identification;
+    }
+
+    /**
+     * Returns the person's phone number.
+     *
+     * @return the person's phone number
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * Updates the person's phone number.
+     *
+     * @param phone the new phone number
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }

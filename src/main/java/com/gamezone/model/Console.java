a74c0be17b0cc3c8eaee2b7c8c0cffb4688b3895
Sherly Corrales <scorrales@unicesar.edu.co>
2026-09-06 11:48:28 -0500

    feat: implement VideoGame and Console classes

---
+++ src/main/java/com/gamezone/model/Console.java
@@ -0,0 +1,33 @@
+package gamezone.model;
+
+public class Console extends Product {
+
+    private String brand;
+    private String model;
+    private String generation;
+
+    public Console(
+            String identifier,
+            String title,
+            double price,
+            int availableQuantity,
+            String brand,
+            String model,
+            String generation) {
+
+        super(identifier, title, price, availableQuantity);
+
+        this.brand = brand;
+        this.model = model;
+        this.generation = generation;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Console: " + getTitle()
+                + " Brand: " + brand
+                + " Model: " + model
+                + " Generation: " + generation;
+    }
+
+}
---
+++ src/main/java/com/gamezone/model/VideoGame.java
@@ -0,0 +1,33 @@
+package gamezone.model;
+
+public class VideoGame extends Product {
+
+    private String platform;
+    private String genre;
+    private String ageRating;
+
+    public VideoGame(
+            String identifier,
+            String title,
+            double price,
+            int availableQuantity,
+            String platform,
+            String genre,
+            String ageRating) {
+
+        super(identifier, title, price, availableQuantity);
+
+        this.platform = platform;
+        this.genre = genre;
+        this.ageRating = ageRating;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Video Game: " + getTitle()
+                + " Platform: " + platform
+                + " Genre: " + genre
+                + " Rating: " + ageRating;
+    }
+
+}

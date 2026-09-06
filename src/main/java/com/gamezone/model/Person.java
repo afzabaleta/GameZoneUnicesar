bcfcc57c653bfa88452a648ed7b09e31003d5ac5
damestre <diegomestre98@gmail.com>
2026-09-06 18:02:49 -0500

    feat: create Person abstract class with common attributes

---
+++ src/main/java/com/gamezone/model/Person.java
@@ -0,0 +1,11 @@
+package com.gamezone.model;
+
+/**
+ * Represents the common information shared by people in the GameZone system.
+ */
+public abstract class Person {
+
+    private String name;
+    private String identification;
+    private String phone;
+}
